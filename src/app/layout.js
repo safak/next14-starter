856acab67d49c6b484053f80b320f02cf94bb677
Safak <lama@dev.local>
2023-12-11 15:42:27 +0100

    commit

---
+++ src/app/layout.js
@@ -0,0 +1,17 @@
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Next App',
+  description: 'Next.js starter app',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>Hello World!</div>;
+};
+
+export default Home;
