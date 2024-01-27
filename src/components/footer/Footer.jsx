58ad1d9bee3ddf7922c167626a7f8a122f1f9f41
jimmy <49584328+JimmyFu-Coder@users.noreply.github.com>
2024-01-27 21:58:04 +0800

    update: footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,6 +1,13 @@
+import styles from "./footer.module.css";
+
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className={styles.container}>
+            <div className={styles.logo} >logo</div>
+            <div  className={styles.text} >
+                test @ All rights reserved
+            </div>
+        </div>
     )
 }
 export default Footer
