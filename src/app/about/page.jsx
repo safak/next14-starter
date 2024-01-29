636171dc54ebf0157ca086ab8cff1db72fece1da
username <useremail@qq.com>
2024-01-29 13:48:08 +0800

    update: about pages

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -4,8 +4,32 @@ import styles from "./about.module.css";
 const AboutPage = () => {
   return (
     <div>
-      <div className={styles.imgContainer}>
-        <Image src="/about.png" alt="" fill />
+      <div className={styles.container}>
+        <div className={styles.textContainer}>
+          <h2 className={styles.subtitle}>About Agency</h2>
+          <h1 className={styles.title}>Better digital ideas</h1>
+          <p className={styles.desc}>
+            test test test
+          </p>
+          <div className={styles.boxes}>
+            <div className={styles.box}>
+              <h1>10 K+</h1>
+              <p>Year of experienc</p>
+            </div>
+            <div className={styles.box}>
+              <h1>10 K+</h1>
+              <p>Year of experienc</p>
+            </div>
+            <div className={styles.box}>
+              <h1>10 K+</h1>
+              <p>Year of experienc</p>
+            </div>
+          </div>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image src="/about.png" alt="About Image" fill className={styles.img} />
+        </div>
+
       </div>
     </div>
   );
