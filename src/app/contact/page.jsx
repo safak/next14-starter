a2291eaef0296ca9487bb938007ddfb484038dc1
username <useremail@qq.com>
2024-01-29 14:12:02 +0800

    update: contact pages

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,6 +1,23 @@
+import Image from 'next/image'
+import styles from './contact.module.css'
+
+
 const ContactPage = () => {
     return (
-        <div>ContactPage</div>
+        <div className={styles.container}>
+            <div className={styles.imgContainer}>
+                <Image src="/contact.png" alt="" fill className={styles.img} />
+            </div>
+            <div className={styles.formContainer}>
+                <form action="" className={styles.form}>
+                    <input type="text" placeholder='Name and Surname' />
+                    <input type="text" placeholder='Email Address' />
+                    <input type="text" placeholder='Phone number' />
+                    <textarea name="" id="" cols="30" rows="10" placeholder='message'></textarea>
+                    <button>Send</button>
+                </form>
+            </div>
+        </div>
     )
 }
 
