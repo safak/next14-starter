b21c3c85f6b99ac76c6babf128ec76b1242a7f20
canutoilberto <canutoilberto@gmail.com>
2024-02-23 16:45:26 -0300

    Contact page added

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,5 +1,29 @@
+import Image from "next/image";
+import styles from "./contact.module.css";
+
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.imageContainer}>
+        <Image src="/contact.png" alt="" fill className={styles.img} />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="Name and surname" />
+          <input type="email" placeholder="Email address" />
+          <input type="phone" placeholder="Phone number (Optional)" />
+          <textarea
+            name=""
+            id=""
+            cols="30"
+            rows="10"
+            placeholder="Message"
+          ></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default ContactPage;
