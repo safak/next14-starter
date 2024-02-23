fadafaf518d7e0bce091f2a3ff2258545e5fee00
canutoilberto <canutoilberto@gmail.com>
2024-02-23 16:07:21 -0300

    About page added

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -6,18 +6,39 @@ const AboutPage = () => {
     <div>
       <div className={styles.container}>
         <div className={styles.textContainer}>
-          <h2>About Agency</h2>
-          <h1>
+          <h2 className={styles.subtitle}>About Agency</h2>
+          <h1 className={styles.title}>
             We create digital ideas that are bigger, bolder, braver and better.
           </h1>
-          <p>
+          <p className={styles.desc}>
             We create digital ideas that are bigger, bolder, braver and better.
             We believe in good ideas flexibility and precission We&apos;re
             world&apos;s Our Special Team best consulting & finance solution
             provider. Wide range of web and software development services.
           </p>
+          <div className={styles.boxes}>
+            <div className={styles.box}>
+              <h1>10 k+</h1>
+              <p>Years of Experience</p>
+            </div>
+            <div className={styles.box}>
+              <h1>234 k+</h1>
+              <p>People reached</p>
+            </div>
+            <div className={styles.box}>
+              <h1>5 k+</h1>
+              <p>Services and plugins</p>
+            </div>
+          </div>
         </div>
-        <div className={styles.imageContainer}></div>
+        <div className={styles.imageContainer}>
+          <Image
+            src="/about.png"
+            alt="Picture of Author"
+            fill
+            className={styles.img}
+          />
+        </div>
       </div>
     </div>
   );
