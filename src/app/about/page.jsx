1a5cc2295e688672a235b1c384ac2fd9dda4ee84
Anastasiia <anastasiia.perih@gmail.com>
2024-02-26 12:52:21 -0500

    next images configuration

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import styles from "./about.module.css";
 
 const AboutPage = () => {
   return (
-    <div>AboutPage</div>
-  )
-}
+    <div>
+      <div className={styles.imgContainer}>
+        <Image src="https://images.pexels.com/photos/20326813/pexels-photo-20326813/free-photo-of-a-city-skyline-with-a-large-building-in-the-middle.png?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" fill />
+      </div>
+    </div>
+  );
+};
 
-export default AboutPage
+export default AboutPage;
