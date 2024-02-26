b798e482f664ba36181ba5333b47fac38ea96b7c
Anastasiia <anastasiia.perih@gmail.com>
2024-02-26 13:11:09 -0500

    logo link fixed

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Links from "./links/Links";
 import styles from './navbar.module.css'
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.logo}>Logo</div>
+      <Link href="/" className={styles.logo}>Logo</Link>
       <Links />
     </div>
   );
