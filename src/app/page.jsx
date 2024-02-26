251c9937b1c4cb84685d7503ba4a9510c7897e1a
Anastasiia <anastasiia.perih@gmail.com>
2024-02-26 13:29:36 -0500

    homepage styling

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,8 +4,8 @@ import Image from 'next/image';
 const Home = () => {
   return <div className={styles.container}>
     <div className={styles.textContainer}>
-      <h1>Creative Thoughts Agency</h1>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt neque, deserunt praesentium consectetur maxime quam et dignissimos optio, aspernatur aut at a iure velit consequatur voluptas ab cumque distinctio delectus.</p>
+      <h1 className={styles.title}>Creative Thoughts Agency</h1>
+      <p className={styles.desc}>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sunt neque, deserunt praesentium consectetur maxime quam et dignissimos optio, aspernatur aut at a iure velit consequatur voluptas ab cumque distinctio delectus.</p>
       <div className={styles.buttons}>
         <button className={styles.button}>Learn More</button>
         <button className={styles.button}>Contact</button>
