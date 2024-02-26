8e7d2c821c77dc97aa4bd239136e1ace64c336a4
Anastasiia <anastasiia.perih@gmail.com>
2024-02-25 21:32:47 -0500

    mobile menu markup

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,44 +1,57 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import styles from "./links.module.css";
 import NavLink from "./navLink/NavLink";
 
+const links = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "About",
+    path: "/about",
+  },
+  {
+    title: "Contact",
+    path: "/contact",
+  },
+  {
+    title: "Blog",
+    path: "/blog",
+  },
+];
+
 const Links = () => {
-  const links = [
-    {
-      title: "Home",
-      path: "/",
-    },
-    {
-      title: "About",
-      path: "/about",
-    },
-    {
-      title: "Contact",
-      path: "/contact",
-    },
-    {
-      title: "Blog",
-      path: "/blog",
-    },
-  ];
+  const [open, setOpen] = useState(false);
 
   //TEMPORARY VARIABLES
   const isAdmin = true;
   const sesstion = true;
 
   return (
-    <div className={styles.links}>
-      {links.map(({ title, path }) => (
-        <NavLink key={path} path={path} title={title} />
-      ))}
-      {sesstion ? (
-        <>
-          {isAdmin && <NavLink key={"/admin"} path="/admin" title={"Admin"} />}
-          <button className={styles.logout}>Logout</button>
-        </>
-      ) : (
-        <NavLink key={"/login"} path="/login" title={"Login"} />
-      )}
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map(({ title, path }) => (
+          <NavLink key={path} path={path} title={title} />
+        ))}
+        {sesstion ? (
+          <>
+            {isAdmin && (
+              <NavLink key={"/admin"} path="/admin" title={"Admin"} />
+            )}
+            <button className={styles.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLink key={"/login"} path="/login" title={"Login"} />
+        )}
+      </div>
+      <button onClick={() => setOpen(prev => !prev)}>Menu</button>
+      {
+        open && <div className={styles.mobileLinks}>
+          {links.map(({path, title}) => <NavLink key={path} path={path} title={title} />)}
+        </div>
+      }
     </div>
   );
 };
