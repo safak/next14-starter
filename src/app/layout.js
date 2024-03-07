8f655cb240b8dff2ea15cfbd37b4efd2b1412695
DenzelMswazi <denzelmwangi94@gmail.com>
2024-03-07 15:34:05 +0300

    updated the imports in the layout folder

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/navbar/Navbar'
+import Footer from '@/components/footer/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <Navbar />
         {children}
+        <Footer />
         </body>
     </html>
   )
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,6 @@
+const Footer = () => {
+    return (
+        <div>Footer</div>
+    )
+}
+export default Footer
