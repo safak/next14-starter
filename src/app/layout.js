45c02e636ce5d4c2ff06c9cd4657c8279cc32e6d
DenzelMswazi <denzelmwangi94@gmail.com>
2024-03-08 15:50:44 +0300

    Added global styling

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,9 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <div className="container">
         <Navbar />
         {children}
         <Footer />
+        </div>
         </body>
     </html>
   )
