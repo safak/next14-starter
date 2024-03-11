78b2fa03d0535b031234c6f07ee94404be47a019
JesZ-Ram <ramirez.jeman92@gmail.com>
2024-03-10 19:17:31 -0700

    added some more components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,8 @@
 const Home = () => {
-  return <div>Hello World!</div>;
+  return <div>Homepage</div>;
 };
 
 export default Home;
+
+
+// Make sure to save all and then run the application...or else it could be using old versions??
